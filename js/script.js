f2050bab059a64b6828a6f9abada38999b39e9d8
ricardoakira <rm551447@fiap.com.br>
2024-05-22 19:50:33 -0300

    mensagem para versionar

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.write("Está é a primeira impressão da tela!")
+console.log("ola");
+
+var nome="fiap"
+console.log(nome);
+
+let nome2="teste"
+console.log(nome2);
+
+const nome3="usuario"
+console.log(nome3);
+
