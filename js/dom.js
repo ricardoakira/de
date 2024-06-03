2411e551b40512ec52da6a60f3a1669b6f562f9b
ricardoakira <rm551447@fiap.com.br>
2024-06-03 19:23:27 -0300

    dom

---
+++ js/dom.js
@@ -0,0 +1,29 @@
+// let titulo = document.getElementsByTagName("h1")
+// console.log(titulo)
+// pega todos os elementos que tem essa tag h1 e printa todos no console
+
+// let titulo = document.getElementsByTagName("h1")[0]
+// console.log(titulo)
+// pega o primeiro elemento que tem a tag h1 da pagina html, a posição está dentro dos parênteses, e a posição 0 é sempre o primeiro elemento
+
+
+// let lis = document.getElementsByTagName("li")
+// console.log(lis)
+// pega todos os elementos que tem a tag li, até os que tem e não tem classes ou id diferente
+
+// let li = document.getElementsByTagName("li")
+// console.log(li[3])
+// ou
+// let li = document.getElementsByTagName("li")[3]
+// console.log(li)
+// pega o elemento da terceira posição da tag li
+
+// let listas = document.getElementById("lista2")
+// console.log(listas)
+// pega o elemento que tem o id chamdo lista2, e printa o que tem dentro dele, até a tag com elementos
+// mas so porde haver um id 
+
+// let paragrafo = document.getElementById("paragrafo")
+// console.log(paragrafo)
+// pega o elemento que possui esse id
+// mas so pode haver um id
