e6910eac2dbbedb4e44b0c8186d5ca3d99fd84b9
ricardoakira <rm551447@fiap.com.br>
2024-06-03 19:45:04 -0300

    querySelector

---
+++ js/query.js
@@ -0,0 +1,22 @@
+// let items = document.querySelectorAll("#lista2 li")
+// console.log(items)
+// # = id
+// a diferença do querySelectorAll e do Id, é que o querySelectorAll não printa o id, apenas o que há dentro dele
+
+// let lista = document.querySelectorAll("#lista .item")
+// console.log(lista)
+// # = id
+// . = class
+// pega os elementos das classes que estão no id
+
+// let lista = document.querySelector("#lista2 li")
+// console.log(lista)
+// igual o querySelectorAll mas ele pega apenas o primeiro elemento da lista que estiver entre parênteses
+
+// let span = document.querySelector("#paragrafo span")
+// console.log(span)
+// printa o span que esta dentro do id paragrafo
+
+// let span = document.querySelector("p span")
+// console.log(span)
+// printa o span que esta dentro do p
