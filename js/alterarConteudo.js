7999e7eeeb24f564c8e3006d94d8fa0b554c2818
ricardoakira <rm551447@fiap.com.br>
2024-06-04 16:14:15 -0300

    Alteração de elementos

---
+++ js/alterarConteudo.js
@@ -0,0 +1,10 @@
+let title = document.querySelector("#title");
+
+// inner
+title.innerHTML = "Este é o texto alterado"
+// faz a alteração do texto da variável title
+
+//textContent
+let subtitle = document.querySelector(".subtitle");
+subtitle.textContent = "Testando o textContent"
+// igual ao innerHTML
